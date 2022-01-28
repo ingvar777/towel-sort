cd3f1c622c3feaa74b1742c0b5b30011e8d99336
ingvar777 <90556370+ingvar777@users.noreply.github.com>
2022-01-28 13:15:22 +0500

    fix: array empty

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 module.exports = function towelSort (matrix) {
 
     let arr = [];
+    
+    if (!matrix) return [];
 
     matrix.forEach((val, indx) => {
         arr = arr.concat((!!(indx % 2)) ? val.reverse() : val)
